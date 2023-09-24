cdc74ac29e4407fe79cc2d9e6674bc2bb6712a92
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-23 18:20:18 -0600

    starting project ApplicationRunner

---
+++ src/test/java/com/application_runner/ApplicationRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
